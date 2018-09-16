98361c998eb2daa360ab17c3f3925573ca012411
Vikash Hart <vikashhart@gmail.com>
2018-09-16 17:26:26 -0400

    added capture session for T2

--- PhotoBooth/CameraView.swift
+++ PhotoBooth/CameraView.swift
@@ -1,1 +1,63 @@
 import UIKit
+
+class CameraView: UIView {
+    
+    lazy var previewLayerContainer: UIView = {
+        let pl = UIView()
+        pl.translatesAutoresizingMaskIntoConstraints = false
+        return pl
+    }()
+    
+    lazy var switchCameraButton: UIButton = {
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "rotate_camera").withRenderingMode(.alwaysTemplate), for: .normal)
+        button.tintColor = .white
+        button.layer.opacity = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        backgroundColor = .white
+        setupViews()
+    }
+    
+    private func setupViews() {
+        setupPreviewLayerContainer()
+        setupSwitchCameraButton()
+    }
+    
+    private func setupPreviewLayerContainer() {
+        addSubview(previewLayerContainer)
+        NSLayoutConstraint.activate([
+            previewLayerContainer.topAnchor.constraint(equalTo: topAnchor),
+            previewLayerContainer.leadingAnchor.constraint(equalTo: leadingAnchor),
+            previewLayerContainer.trailingAnchor.constraint(equalTo: trailingAnchor),
+            previewLayerContainer.centerXAnchor.constraint(equalTo: centerXAnchor),
+            previewLayerContainer.centerYAnchor.constraint(equalTo: centerYAnchor),
+            previewLayerContainer.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+    }
+    
+    private func setupSwitchCameraButton() {
+        previewLayerContainer.addSubview(switchCameraButton)
+        NSLayoutConstraint.activate([
+            switchCameraButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+            switchCameraButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+            switchCameraButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.2),
+            switchCameraButton.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1)
+            ])
+    }
+    
+}
+
--- PhotoBooth/CaptureSession.swift
+++ PhotoBooth/CaptureSession.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AVFoundation
 
 protocol PhotoCaptureable {
     
@@ -7,13 +8,139 @@ protocol PhotoCaptureable {
     func configurePreview(view: UIView)
 }
 
-class CaptureSession: PhotoCaptureable {
+class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate {
+    
+    var avSession = AVCaptureSession()
+    
+    var backCamera: AVCaptureDevice?
+    var frontCamera: AVCaptureDevice?
+    var currentCamera: AVCaptureDevice?
+    
+    var photoOutput: AVCapturePhotoOutput?
+    var videoOutput: AVCaptureVideoDataOutput?
+    let photoSessionPreset = AVCaptureSession.Preset.photo
+    let videoSessionPreset = AVCaptureSession.Preset.medium
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    var cameraView = CameraView()
+    
+    var image: UIImage?
+    var filmStrip = [UIImage]()
+    
+    func setupCaptureSession() {
+        setupPhotoCaptureSession()
+        setupDevices()
+        setUpCaptureSessionInput(position: .front)
+        setupPreviewLayer(view: cameraView.previewLayerContainer)
+        startRunningCaptureSession()
+    }
     
     func getPhotos() -> UIImage {
-        
+        return UIImage()
     }
     
     func configurePreview(view: UIView) {
         
+        cameraPreviewLayer?.frame = view.bounds
     }
+    
+    // This function sets up a switch to change the camera in use depending on current position when called.
+    private func setUpCaptureSessionInput(position: AVCaptureDevice.Position) {
+        switch position {
+        case .back:
+            currentCamera = backCamera
+        case .front:
+            currentCamera = frontCamera
+        default:
+            currentCamera = backCamera
+        }
+        
+        setupInputOutput()
+    }
+    
+    // Mark:- @objc functions for buttons
+    // This is the function that will be called when the take photo button is pressed.
+    @objc func takePhoto(){
+        avSession.beginConfiguration()
+        avSession.sessionPreset = photoSessionPreset
+        avSession.commitConfiguration()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            let settings = AVCapturePhotoSettings()
+            self.photoOutput?.capturePhoto(with: settings, delegate: self as AVCapturePhotoCaptureDelegate)
+            print("photo taken")
+        })
+    }
+    
+    // This function switches the camera and animates the button.
+    @objc func changeCamera(sender: UIButton) {
+        guard let currentPosition = currentCamera?.position else { return }
+        let newPosition: AVCaptureDevice.Position = currentPosition == .back ? .front : .back
+        setUpCaptureSessionInput(position: newPosition)
+        print("camera switched")
+    }
+    
+    // Mark:- AVCapture Session Setup functions
+    // This function sets up the photo capture session as well as the photo ouput instance and its settings.
+    private func setupPhotoCaptureSession(){
+        photoOutput = AVCapturePhotoOutput()
+        photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
+        avSession.addOutput(photoOutput!)
+    }
+    
+    // This function allows you to have the application discover the devices camera(s)
+    private func setupDevices(){
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
+        let devices = deviceDiscoverySession.devices
+        
+        for device in devices {
+            if device.position == AVCaptureDevice.Position.back {
+                backCamera = device
+            } else if device.position == AVCaptureDevice.Position.front {
+                frontCamera = device
+            }
+        }
+    }
+    
+    // This function allows you to remove the current camera input in use and to set and enable a new camera input.
+    private func setupInputOutput(){
+        avSession.beginConfiguration()
+        if let currentInput = avSession.inputs.first {
+            avSession.removeInput(currentInput)
+        }
+        do {
+            let captureDeviceInput = try AVCaptureDeviceInput(device: currentCamera!)
+            avSession.addInput(captureDeviceInput)
+        } catch {
+            print(error)
+        }
+        avSession.commitConfiguration()
+    }
+    
+    // This function creates a layer in the view that will enable a live feed of what your camera is observing.
+    private func setupPreviewLayer(view: UIView){
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: avSession)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+        view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
+    }
+    
+    // Starts running the capture session after you set up the view.
+    private func startRunningCaptureSession(){
+        avSession.startRunning()
+    }
+    
+    // AVCapturePhotoCaptureDelegate methods. This extension is used because you need to wait until the photo you took "didFinishProcessing" before you can handle the image.
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let imageData = photo.fileDataRepresentation() {
+            print(imageData)
+            image = UIImage(data: imageData)
+//            guard let image = image else { return }
+//            let previewVC = PreviewViewController(image: image)
+//            previewVC.delegate = self
+//            present(previewVC, animated: true, completion: nil)
+
+        }
+    }
+    
 }
