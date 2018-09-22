4d9f133c6bc784225491067467d289351791b4eb
Vikash Hart <vikashhart@gmail.com>
2018-09-22 16:04:43 -0400

    Capture session added and configured for T2 and switch camera button for T5

--- PhotoBooth/CaptureSession.swift
+++ PhotoBooth/CaptureSession.swift
@@ -2,48 +2,46 @@ import UIKit
 import AVFoundation
 
 protocol PhotoCaptureable {
-    
+
     func getPhotos() -> UIImage
-    
+
     func configurePreview(view: UIView)
+
+    func switchCamera()
 }
 
 class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate {
-    
+
     var avSession = AVCaptureSession()
-    
+
     var backCamera: AVCaptureDevice?
     var frontCamera: AVCaptureDevice?
     var currentCamera: AVCaptureDevice?
-    
+
     var photoOutput: AVCapturePhotoOutput?
-    var videoOutput: AVCaptureVideoDataOutput?
     let photoSessionPreset = AVCaptureSession.Preset.photo
-    let videoSessionPreset = AVCaptureSession.Preset.medium
-    
+
     var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
-    var cameraView = CameraView()
-    
+
     var image: UIImage?
     var filmStrip = [UIImage]()
-    
+
     func setupCaptureSession() {
         setupPhotoCaptureSession()
         setupDevices()
         setUpCaptureSessionInput(position: .front)
-        setupPreviewLayer(view: cameraView.previewLayerContainer)
-        startRunningCaptureSession()
     }
-    
+
     func getPhotos() -> UIImage {
         return UIImage()
     }
-    
+
     func configurePreview(view: UIView) {
-        
-        cameraPreviewLayer?.frame = view.bounds
+        setupCaptureSession()
+        setupPreviewLayer(view: view)
+        startRunningCaptureSession()
     }
-    
+
     // This function sets up a switch to change the camera in use depending on current position when called.
     private func setUpCaptureSessionInput(position: AVCaptureDevice.Position) {
         switch position {
@@ -54,32 +52,32 @@ class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate 
         default:
             currentCamera = backCamera
         }
-        
+
         setupInputOutput()
     }
-    
+
     // Mark:- @objc functions for buttons
     // This is the function that will be called when the take photo button is pressed.
     @objc func takePhoto(){
         avSession.beginConfiguration()
         avSession.sessionPreset = photoSessionPreset
         avSession.commitConfiguration()
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
             let settings = AVCapturePhotoSettings()
             self.photoOutput?.capturePhoto(with: settings, delegate: self as AVCapturePhotoCaptureDelegate)
             print("photo taken")
         })
     }
-    
-    // This function switches the camera and animates the button.
-    @objc func changeCamera(sender: UIButton) {
+
+    // This function switches the camera.
+    @objc func switchCamera() {
         guard let currentPosition = currentCamera?.position else { return }
         let newPosition: AVCaptureDevice.Position = currentPosition == .back ? .front : .back
         setUpCaptureSessionInput(position: newPosition)
         print("camera switched")
     }
-    
+
     // Mark:- AVCapture Session Setup functions
     // This function sets up the photo capture session as well as the photo ouput instance and its settings.
     private func setupPhotoCaptureSession(){
@@ -87,12 +85,12 @@ class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate 
         photoOutput?.setPreparedPhotoSettingsArray([AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])], completionHandler: nil)
         avSession.addOutput(photoOutput!)
     }
-    
+
     // This function allows you to have the application discover the devices camera(s)
     private func setupDevices(){
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
         let devices = deviceDiscoverySession.devices
-        
+
         for device in devices {
             if device.position == AVCaptureDevice.Position.back {
                 backCamera = device
@@ -101,7 +99,7 @@ class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate 
             }
         }
     }
-    
+
     // This function allows you to remove the current camera input in use and to set and enable a new camera input.
     private func setupInputOutput(){
         avSession.beginConfiguration()
@@ -116,7 +114,7 @@ class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate 
         }
         avSession.commitConfiguration()
     }
-    
+
     // This function creates a layer in the view that will enable a live feed of what your camera is observing.
     private func setupPreviewLayer(view: UIView){
         cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: avSession)
@@ -124,14 +122,14 @@ class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate 
         cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
         view.layer.insertSublayer(cameraPreviewLayer!, at: 0)
     }
-    
+
     // Starts running the capture session after you set up the view.
     private func startRunningCaptureSession(){
         avSession.startRunning()
     }
-    
+
     // AVCapturePhotoCaptureDelegate methods. This extension is used because you need to wait until the photo you took "didFinishProcessing" before you can handle the image.
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+    private func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
         if let imageData = photo.fileDataRepresentation() {
             print(imageData)
             image = UIImage(data: imageData)
@@ -142,5 +140,5 @@ class CaptureSession: NSObject, PhotoCaptureable, AVCapturePhotoCaptureDelegate 
 
         }
     }
-    
+
 }
---
+++ PhotoBooth/MockSession.swift
@@ -0,0 +1,28 @@
+//
+//  MockSession.swift
+//  PhotoBooth
+//
+//  Created by C4Q on 9/20/18.
+//  Copyright © 2018 Vikash Hart. All rights reserved.
+//
+
+import UIKit
+
+class MockSession: PhotoCaptureable {
+    let mockImage = UIImage()
+    
+    func getPhotos() -> UIImage {
+        return UIImage()
+    }
+    
+    func configurePreview(view: UIView) {
+        
+    }
+    
+    func switchCamera() {
+        
+    }
+    
+    
+    
+}
