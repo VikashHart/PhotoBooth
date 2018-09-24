411d8265449d6628c100f61586c1bc763ee2573c
VikashHart <32072791+VikashHart@users.noreply.github.com>
2018-09-23 22:54:45 -0400

    Copy t2 (#3)
    * Created protocol and VC for ticket 1
    
    * file cleanup
    
    * added capture session for T2
    
    * Capture session added and configured for T2 and switch camera button for T5
    
    * file cleanup with minor changes
    
    * assests added

---
+++ PhotoBooth/CameraViewController.swift
@@ -0,0 +1,79 @@
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    
+    private let captureSession: PhotoCaptureable = CaptureSession()
+    
+    lazy var previewLayerContainer: UIView = {
+        let pl = UIView()
+        pl.translatesAutoresizingMaskIntoConstraints = false
+        return pl
+    }()
+    
+    lazy var switchCameraButton: UIButton = {
+        let button = UIButton()
+        let image = UIImage(named: "rotate_camera")?.withRenderingMode(.alwaysTemplate)
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.layer.opacity = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+        
+    private var capturedImages = [UIImage]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        captureSession.configurePreview(view: previewLayerContainer)
+        self.switchCameraButton.addTarget(self,
+                                                action: #selector(rotateCamera),
+                                                for: .touchUpInside)
+        setupDownSwipeGesture()
+    }
+
+    private func setupDownSwipeGesture() {
+        let down = UISwipeGestureRecognizer(target : self, action : #selector(cancelPhotoBoothSession))
+        down.direction = .down
+        self.view.addGestureRecognizer(down)
+    }
+    
+    @objc private func rotateCamera() {
+        captureSession.switchCamera()
+    }
+    
+    @objc private func cancelPhotoBoothSession() {
+        
+    }
+    
+    private func setupViews() {
+        setupPreviewLayerContainer()
+        setupSwitchCameraButton()
+    }
+    
+    private func setupPreviewLayerContainer() {
+        view.addSubview(previewLayerContainer)
+        NSLayoutConstraint.activate([
+            previewLayerContainer.topAnchor.constraint(equalTo: view.topAnchor),
+            previewLayerContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            previewLayerContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            previewLayerContainer.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            ])
+    }
+    
+    private func setupSwitchCameraButton() {
+        previewLayerContainer.addSubview(switchCameraButton)
+        NSLayoutConstraint.activate([
+            switchCameraButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            switchCameraButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            switchCameraButton.widthAnchor.constraint(equalToConstant: 40),
+            switchCameraButton.heightAnchor.constraint(equalToConstant: 40)
+            ])
+    }
+
+}
+
+extension CameraViewController: UIGestureRecognizerDelegate {
+    
+}
---
+++ PhotoBooth/MockSession.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class MockSession: PhotoCaptureable {
+    let mockImage = UIImage()
+    
+    func captureImage() -> UIImage {
+        return UIImage()
+    }
+    
+    func configurePreview(view: UIView) {
+        
+    }
+    
+    func switchCamera() {
+        
+    }
+    
+    
+    
+}
